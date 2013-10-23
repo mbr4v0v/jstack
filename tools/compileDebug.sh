56fdf5f01805b0c6e028ebcc49f1da920a715b88
Javier Cerviño <jcervino@dit.upm.es>
2013-10-23 16:52:08 +0200

    Changed Nova.Volume to Cinder

--- tools/compileDebug.sh
+++ tools/compileDebug.sh
@@ -6,5 +6,5 @@ cat ../src/JSTACK.Comm.js >> $OUTPUT
 cat ../src/JSTACK.Utils.js >> $OUTPUT
 cat ../src/JSTACK.Keystone.js >> $OUTPUT
 cat ../src/JSTACK.Nova.js >> $OUTPUT
-cat ../src/JSTACK.Nova.Volume.js >> $OUTPUT
+cat ../src/JSTACK.Cinder.js >> $OUTPUT
 cat ../src/JSTACK.Glance.js >> $OUTPUT
